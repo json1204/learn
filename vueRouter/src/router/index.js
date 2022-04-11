df31dec0c4f10b893b17fb1b8242c6f93224f1d9
unknown <390149386@qq.com>
2022-04-11 20:08:39 +0800

    sd

---
+++ vueRouter/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from './myVueRouter' //修改代码
+import Home from '../views/Home.vue'
+import About from "../views/About.vue"
+Vue.use(VueRouter)
+  const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+];
+const router = new VueRouter({
+  mode:"hash",
+  routes
+})
+export default router
